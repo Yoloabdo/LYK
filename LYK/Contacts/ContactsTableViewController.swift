6ac5667ae1107b9963b49a2ac16d09a3d0b60c59
yoloabdo <dhooom.da@gmail.com>
2017-12-02 04:23:05 +0200

    initial login done, friends request as well

---
+++ LYK/Contacts/ContactsTableViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ContactsTableViewController.swift
+//  LYK
+//
+//  Created by abdelrahman mohamed on 12/2/17.
+//  Copyright © 2017 abdelrahman mohamed. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+class ContactsTableViewController: UITableViewController {
+
+    struct StoryBoard {
+        static let LoginSegue = "PresentLogin"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Check if user logged in, else present login.
+        if FBSDKAccessToken.current() == nil {
+            performSegue(withIdentifier: StoryBoard.LoginSegue, sender: nil)
+        }
+        
+        // add search
+        
+        // add offline maybe? 
+        
+        
+        getContacts()
+        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+    func getContacts() -> Void {
+        // For more complex open graph stories, use `FBSDKShareAPI`
+        // with `FBSDKShareOpenGraphContent`
+        /* make the API call */
+        let request = FBSDKGraphRequest.init(graphPath: "/{friend-list-id}", parameters: nil, httpMethod: "GET")
+        
+        _ = request?.start(completionHandler: { (request, results, error) in
+            print(results)
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LYK/Login/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  LYK
+//
+//  Created by abdelrahman mohamed on 12/2/17.
+//  Copyright © 2017 abdelrahman mohamed. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var fbLoginBtn: FBSDKLoginButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fbLoginBtn.readPermissions = ["public_profile", "email", "user_friends", "read_custom_friendlists"]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension LoginViewController: FBSDKLoginButtonDelegate {
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        guard (result) != nil else {
+            return
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    
+}
